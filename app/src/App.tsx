fe7e9281f13f33c6de814da1dd0a7f562947e1dd
SimonBerens <sberensnyc@gmail.com>
2021-08-03 23:36:14 -0400

    mvp?

---
+++ app/src/App.tsx
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react';
+import {io} from "socket.io-client";
+import './App.css';
+import ReactPlayer from 'react-player';
+
+function App() {
+
+    const [videoIdxQueue] = useState<Array<number>>([]);
+    const [playingDefault, setPlayingDefault] = useState<boolean>(true);
+    const [counter, setCounter] = useState<number>(0);
+
+    useEffect(() => {
+        const socket = io("http://localhost:5000");
+        socket.on('video_queued', ({video_idx}) => {
+            videoIdxQueue.push(video_idx);
+        });
+    });
+    console.log(videoIdxQueue);
+    return (
+        <div className="App">
+            <ReactPlayer
+                key={counter}
+                controls
+                playing
+                url={[
+                    {src: `video${videoIdxQueue[0] ?? 0}.webm`, type: 'video/webm'},
+                ]}
+                onEnded={() => {
+                    console.log('ended');
+                    if (!playingDefault) {
+                        videoIdxQueue.splice(0, 1);
+                    }
+                    setPlayingDefault(videoIdxQueue.length === 0);
+                    setCounter(counter + 1);
+                }}
+            />
+        </div>
+    );
+}
+
+export default App;
