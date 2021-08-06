0e08859b65424208e44771157fd5388b3ade8d50
SimonBerens <sberensnyc@gmail.com>
2021-08-05 20:51:25 -0400

    remove duplicate responses within 1 minute of each other

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO
 from werkzeug.exceptions import BadRequest
 from google.protobuf.json_format import MessageToDict
 from dialogflow import get_response
-
+from collections import defaultdict
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -17,6 +17,10 @@ def queue_video():
     return "success"
 
 
+before_filter = defaultdict(lambda: time.time_ns())
+after_filter = defaultdict(lambda: time.time_ns())
+
+
 @app.route("/api/rawtext", methods=["GET", "POST"])
 def handle_raw_text():
     body = request.get_json(force=True)
@@ -24,8 +28,18 @@ def handle_raw_text():
     if not raw_text:
         raise BadRequest("Expected a rawText field in JSON body")
     print(f"Got raw text: {raw_text}")
+
+    if time.time_ns() - before_filter[raw_text] < 60 * 10 ** 9:
+        return ""
+    before_filter[raw_text] = time.time_ns()
+
     response = get_response(raw_text)
     resp = jsonify(MessageToDict(response))
+
+    if time.time_ns() - after_filter[resp] < 60 * 10 ** 9:
+        return ""
+    after_filter[resp] = time.time_ns()
+
     # TODO: call queue_video with the video ID from response
     return resp
 
